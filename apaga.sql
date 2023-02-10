514ea77ff2756ffc8a6f5863827b7875437ead7f
Victor Rayan <77134807+victor-rayan@users.noreply.github.com>
2023-02-10 18:45:53 -0300

    Update apaga.sql

--- apaga.sql
+++ apaga.sql
@@ -1,3 +1,19 @@
+-- ------------------------------   << InstacartMarketBasket (Trabalho Final)  >>   ------------------------------
+--
+--                                                SCRIPT DE APAGAR (DDL)                                                
+-- 
+-- Data Criação ...........: 10/02/2023
+-- Autor(es) ..............: Victor, Deivid, Paulo
+-- Banco de Dados .........: MySQL 8.0
+-- Banco de Dados(nome) ...: InstacartMarketBasket
+-- 
+--
+-- PROJETO => 01 Base de Dados
+--         => 06 Tabelas
+--         => 01 Usuários
+-- 
+-- ------------------------------------------------------------------------------------------------------------------------
+
 USE InstacartMarketBasket;
 
 -- Apagando todas Tabelas
